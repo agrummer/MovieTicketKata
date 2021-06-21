1d7fee66f25081d665b3c2213191027c7886e671
Alex Grummer <alex@agrummer.com>
2021-06-21 14:44:14 -0700

    Prep for candidate

--- src/main/java/de/saxsys/dojo/ticketkata/CinemaCashRegister.java
+++ src/main/java/de/saxsys/dojo/ticketkata/CinemaCashRegister.java
@@ -1,11 +1,6 @@
 package de.saxsys.dojo.ticketkata;
 
 import java.io.IOException;
-import java.text.NumberFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 /**
  * CinemaCashRegister supports sales transactions for bundles of movie tickets.
@@ -14,8 +9,6 @@ import java.util.List;
  * @version 0.1
  */
 public class CinemaCashRegister {
-
-    List<Purchase> purchases = new ArrayList<>();
 
     /**
      * (1) New customers arrive at your ticket booth and tell you
@@ -29,7 +22,7 @@ public class CinemaCashRegister {
     public void startPurchase(String movieTitle, int movieRuntime, Day day, boolean parquet) {
         Movie movie = new Movie(movieTitle, movieRuntime);
         Purchase purchase = new Purchase(movie, day, parquet);
-        purchases.add(purchase);
+
     }
 
     /**
@@ -40,7 +33,7 @@ public class CinemaCashRegister {
      */
     public void addTicket(int age, boolean isStudent) {
         Person person = new Person(age, isStudent);
-        getCurrentPurchase().addPerson(person);
+
     }
 
     /**
@@ -49,7 +42,7 @@ public class CinemaCashRegister {
      * @return total in dollars
      */
     public float finishPurchase() {
-        return getCurrentPurchase().calcTotalPrice();
+        return 0.0f;
     }
 
     /**
@@ -62,28 +55,7 @@ public class CinemaCashRegister {
      *   - Total sales revenue as the last row
      */
     public void writeAllPurchasesToCSV() throws IOException {
-        List<String[]> rows = new ArrayList<>();
-        rows.add(new String[]{"Day", "Movie", "Tickets", "Total"});
-        int totalTickets = 0;
-        float totalSalesRevenue = 0.0f;
-        for (Purchase purchase : purchases) {
-            Day day = purchase.getDay();
-            String movieTitle = purchase.getMovie().getTitle();
-            int tickets = purchase.getPeople().size();
-            float purchasePrice = purchase.calcTotalPrice();
-            rows.add(new String[]{day.name(), movieTitle, String.valueOf(tickets), NumberFormat.getCurrencyInstance().format(purchasePrice)});
-            totalTickets += tickets;
-            totalSalesRevenue += purchasePrice;
-        }
-        rows.add(new String[]{"", "Total Sales Revenue", String.valueOf(totalTickets), NumberFormat.getCurrencyInstance().format(totalSalesRevenue)});
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        String filename = dateFormat.format(new Date()) + ".csv";
-        CsvUtil.writeToCsvFile(filename, rows);
-    }
-
-    private Purchase getCurrentPurchase() {
-        return purchases.get(purchases.size() - 1);
     }
 
 }
